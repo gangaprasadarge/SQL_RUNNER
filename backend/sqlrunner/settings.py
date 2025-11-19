81c0bbfe3e6e0422f0acb1af59338a4b6564fe2c
ARGE GANGA PRASAD <132547894+gangaPrasad990@users.noreply.github.com>
2025-11-19 23:36:34 +0530

    all done

--- backend/sqlrunner/settings.py
+++ backend/sqlrunner/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'api',
+    'anymail',
    
 ]
 
@@ -137,22 +138,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# # Email settings (ensure these are set securely via environment variables in production)
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
-# EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() in ("true", "1", "yes")
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "") # Set this in Render env vars for production
-# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "") # Set this in Render env vars for production
 
+EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+ANYMAIL = {
+    "SENDGRID_API_KEY": os.environ.get("EMAIL_HOST_PASSWORD"),
+}
+
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+SERVER_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 
 
